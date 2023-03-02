a8b7b869d36c271a73f319ad1f0899b485848bba
rinkuberde <rinkuberde@gmail.com>
2023-03-02 20:50:07 +0530

    Welcome Message

---
+++ MultiThreading-Approach/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiThreading_Approach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Employee Payroll using MultiThreading-Approach!!!");
+        }
+    }
+}
