57d4d276b5271b5511f89393a5892b39fde7841d
rinkuberde <rinkuberde@gmail.com>
2023-03-02 21:10:10 +0530

    Employee Payroll Using MultiThreading

--- MultiThreading-Approach/Program.cs
+++ MultiThreading-Approach/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Employee Payroll using MultiThreading-Approach!!!");
+            string[] words = Demo.CreateWordArray(@"http://www.gutenberg.org/files/54700/54700-0.txt");
         }
     }
 }
